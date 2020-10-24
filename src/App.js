ac63d2e8c814431426df8821792277064f4e3617
perovskite13 <mooyy94@gmail.com>
2020-10-24 11:53:39 +0800

    first transactions page setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from "./components/Nav/Nav";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import HomePage from "./pages/HomePage/HomePage";
+import "./App.css";
+import TransactionsHistoryPage from "./pages/TransactionsHistoryPage/TransactionsHistoryPage";
+import TransactionsPage from "./pages/TransactionsPage/TransactionsPage";
+
+
+function App(){
+
+  return(
+    <div className = "App">
+      <div>
+      
+        <Router>
+        <div >
+          <Nav className = "nav" />
+          <Switch>
+
+          
+          <Route path="/transactionshistory">
+            <TransactionsHistoryPage />
+          </Route>
+
+          <Route path="/home">
+            <TransactionsPage />
+          </Route>
+
+
+          <Route path="/">
+            <HomePage />
+          </Route>
+          
+        </Switch>
+        </div>
+        </Router>
     </div>
-  );
+    </div>
+  )
 }
 
 export default App;
