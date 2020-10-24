bcf81559e4f91f0eb5330ef81a8d227a2f0909db
peidcud <51078365+peidcud@users.noreply.github.com>
2020-10-24 17:26:11 +0800

    Update App.js to fix react-router-dom error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
 
