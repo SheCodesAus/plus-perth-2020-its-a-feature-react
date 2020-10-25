c252b9837d94d0e2e3da985d89e6146740bc84c0
Phobacloh <anna.lowery@hotmail.com>
2020-10-25 09:15:46 +0800

    Logo placeholder on all pages (App.js) Headings, fonts and sizing and colour set.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import Nav from "./components/Nav/Nav";
 import SideNavBar from "./components/SideNavBar/SideNavBar";
@@ -6,39 +6,38 @@ import HomePage from "./pages/HomePage/HomePage";
 import "./App.css";
 import TransactionsHistoryPage from "./pages/TransactionsHistoryPage/TransactionsHistoryPage";
 import TransactionsPage from "./pages/TransactionsPage/TransactionsPage";
+import Logo from "../src/logo-placeholder.jpg";
 
+function App() {
+  return (
+    <div>
+      <div id="logo_container">
+        <img className="logo" src={Logo} alt="Buckets Logo" />
+      </div>
+      <div className="App">
+        <div>
+          <Router>
+            <div>
+              <SideNavBar className="nav" />
+              <Switch>
+                <Route path="/TransactionsHistoryPage">
+                  <TransactionsHistoryPage />
+                </Route>
 
-function App(){
+                <Route path="/TransactionsPage">
+                  <TransactionsPage />
+                </Route>
 
-  return(
-    <div className = "App">
-      <div>
-      
-        <Router>
-        <div >
-          <SideNavBar className = "nav" />
-          <Switch>
-
-          
-          <Route path="/TransactionsHistoryPage">
-            <TransactionsHistoryPage />
-          </Route>
-
-          <Route path="/TransactionsPage">
-            <TransactionsPage />
-          </Route>
-
-
-          <Route path="/">
-            <HomePage />
-          </Route>
-          
-        </Switch>
+                <Route path="/">
+                  <HomePage />
+                </Route>
+              </Switch>
+            </div>
+          </Router>
         </div>
-        </Router>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
 export default App;
