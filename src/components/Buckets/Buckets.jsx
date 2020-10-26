461283bb387a186b0ee79d5b2a0313d5ebf25bc1
Phobacloh <anna.lowery@hotmail.com>
2020-10-26 18:15:07 +0800

    Added Bucket component to transaction page, added placeholder hardcoded info, placeholder icon for Delete option, Placeholder Bucket image

---
+++ src/components/Buckets/Buckets.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "../Buckets/Buckets.css";
+import Bucket_img from "../../assets/images/bucket.png";
+import Delete from "../../assets/images/delete.png";
+
+function Bucket() {
+  return (
+    <div className="bucket">
+      <img className="bucket-pic" alt="Bucket Image" src={Bucket_img} />
+      {/* <img className="icon" alt="icon" src= /> */}
+      {/* icon URLS */}
+      <h2>Account Name</h2>
+      <h3>Minimum Amount</h3>
+      <h3>%</h3>
+      <p id="description">Description</p>
+      <div>
+        <div>
+          {/* <Link to={`/delete/${id}`}> */}
+          <a>
+            <img src={Delete} alt="Bin image" height={30}></img>
+          </a>
+          {/* </Link> */}
+        </div>
+      </div>
+
+      {/* <EditBucketForm project_id={id} /> */}
+    </div>
+  );
+}
+
+export default Bucket;
