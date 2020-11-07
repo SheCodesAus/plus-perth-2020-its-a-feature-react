fdaeac18f87b71d437c4a0cbce49d30e7d4fcd1c
Phobacloh <anna.lowery@hotmail.com>
2020-11-07 10:00:02 +0800

    Typo fix

--- src/components/Delete/DeleteTransHistory.jsx
+++ src/components/Delete/DeleteTransHistory.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useHistory, useParams } from "react-router-dom";
 
-function DeleteTransationHistory() {
+function DeleteTransactionHistory() {
   const history = useHistory();
   const { id } = useParams();
 
@@ -41,4 +41,4 @@ function DeleteTransationHistory() {
   );
 }
 
-export default DeleteTransationHistory;
+export default DeleteTransactionHistory;
