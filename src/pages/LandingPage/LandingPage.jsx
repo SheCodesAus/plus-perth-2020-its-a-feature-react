1fca55d8964c2ecb8ad995266c197855df5d0f33
perovskite13 <mooyy94@gmail.com>
2020-11-13 02:15:37 +0800

    Update LandingPage.jsx

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -3,11 +3,11 @@ import RecordTransaction from "../../assets/gif/RecordTransaction.gif"
 
 function LandingPage(){
     return(
-        <div >
+        <div className="landing-text animated fadeInLeft">
             <h1 id="text-head">Explore the features now</h1>
             <div>
-            <div className="landing">
-            <div className="landing-text">
+            <div className="landing animated fadeInLeft">
+            <div className="landing-text animated fadeInLeft">
                 <h1>WELCOME</h1>
             </div>
             <img src={RecordTransaction} height={1000}/>
