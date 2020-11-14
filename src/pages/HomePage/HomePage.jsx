c7f2a6e1266d9ce3bb9e19fe6ccca8d9c46d6cea
Phobacloh <anna.lowery@hotmail.com>
2020-11-14 11:47:21 +0800

    Add bucket link and logo!

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,6 +3,8 @@ import Bucket from "../../components/Buckets/Buckets";
 import LoginForm from "../../components/LoginForm/LoginForm";
 import IncomeForm from "../../components/Buckets/IncomeForm";
 import LandingPage from "../LandingPage/LandingPage";
+import Add from "../../assets/images/add.png";
+import ReactTooltip from "react-tooltip";
 
 function HomePage() {
   const [bucketList, setBucketList] = useState([]);
@@ -48,7 +50,16 @@ function HomePage() {
               );
             })
             :
-            <h2>"Add bucket"</h2>
+            <a
+            className="big-add-bucket-box"
+            href="/addbucket"
+            data-tip="Add new bucket"
+            data-for="AddTip"
+          >
+            <h2>Add Bucket</h2>
+            <img className= "big-add-buckets" src={Add} alt="Add" height={30}></img>
+            <ReactTooltip id="AddTip" />
+          </a>
       }
           </div>
         </div>
